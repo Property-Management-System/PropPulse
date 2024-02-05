31285da2493e79512ff5cf73826f5b3b29d4aed0
Rimah Houssameldine <rimahhd3@gamil.com>
2024-02-05 19:24:16 +0200

    add footer

--- backend/doc/assets/inc/footer1.php
+++ backend/doc/assets/inc/footer1.php
@@ -1,3 +1,3 @@
 <footer class="footer footer-alt">
-            2023 - <?php echo date ('Y');?> &copy; Developed By HMS Team at Park Inovation</a> 
+            2023 - <?php echo date ('Y');?> &copy; Developed By ParkPulse Team at Park Innovation</a> 
 </footer>
